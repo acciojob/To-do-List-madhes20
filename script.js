121135e4e9b228aa01991fc060af90f872ef06c7
madhes20 <118300068+madhes20@users.noreply.github.com>
2023-01-18 19:22:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 //your code here
+let inputValue = document.querySelector("#newTodoInput");
+let inputBtn = document.querySelector("#addTodoBtn");
+let orderTag = document.querySelector("#todoList");
 
+inputBtn.addEventListener("click",addList);
+
+function addList(){
+    let listItem = document.createElement("li");
+    listItem.append(inputValue.value);
+    orderTag.append(listItem);
+}
